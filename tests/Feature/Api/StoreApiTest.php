c824890ad366dd259fd259014d83f0681c63fe0c
aguillarsan <saguilarsanhuza@gmail.com>
2022-12-29 18:00:05 -0400

    test api

--- tests/Feature/Api/StoreApiTest.php
+++ tests/Feature/Api/StoreApiTest.php
@@ -8,17 +8,58 @@ use Tests\TestCase;
 
 class StoreApiTest extends TestCase
 {
+    use RefreshDatabase;
     /**
      * A basic feature test example.
      *
      * @return void
      */
-    public function test_example()
+    public function test_stores()
     {
-        $response = $this->getJson(route('api.stores.index'));
-		// We will only assert that the response returns a 200 
-		// status for now.
-		$response->assertOk(); 
+
+
+        $response = $this->getJson('/api/stores');
+        $response->assertOk();
+        $response->assertJson([
+
+            'stores' => [
+                'current_page' => 1,
+                'data' => [],
+                "first_page_url" =>  "http://localhost/api/stores?page=1",
+                "from" =>  null,
+                "last_page" =>  1,
+                "last_page_url" =>  "http://localhost/api/stores?page=1",
+                "links" =>  [
+                    [
+                        "url" =>  null,
+                        "label" =>  "&laquo; Previous",
+                        "active" =>  false
+                    ],
+                    [
+                        "url" =>  "http://localhost/api/stores?page=1",
+                        "label" =>  "1",
+                        "active" =>  true
+                    ],
+                    [
+                        "url" =>  null,
+                        "label" =>  "Next &raquo;",
+                        "active" =>  false
+                    ]
+                ],
+                "next_page_url" => null,
+                "path" => "http://localhost/api/stores",
+                "per_page" => 20,
+                "prev_page_url" => null,
+                "to" => null,
+                "total" => 0
+            ]
+
+        ]);
+    }
+    public function test_show_store()
+    {
+        $response = $this->getJson('/api/stores/1');
+        $response->assertOk();
 
     }
 }
--- tests/Unit/Api/StoreApiTest.php
+++ tests/Unit/Api/StoreApiTest.php
@@ -11,7 +11,7 @@ class StoreApiTest extends TestCase
      *
      * @return void
      */
-    public function test_example()
+    public function test_stores()
     {
         $this->assertTrue(true);
     }
